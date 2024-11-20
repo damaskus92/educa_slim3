c2b3a1cababb90865ddbfcb8013559f0fb23f1af
damaskus92 <damaskusuma92@gmail.com>
2024-11-21 02:58:13 +0700

    feat: crud student data

--- src/src/routes.php
+++ src/src/routes.php
@@ -99,6 +99,94 @@ return function (App $app) {
     });
 
     $app->get('/students', function (Request $request, Response $response, array $args) use ($container) {
-        return $this->view->render($response, 'student.twig', []);
+        $schools = $this->db->select('tbl_schools', ['id', 'school_name']);
+
+        return $this->view->render($response, 'student.twig', [
+            'schools' => $schools
+        ]);
     })->setName('students');
+
+    $app->group('/api', function () use ($app) {
+        $app->get('/students', function (Request $request, Response $response, array $args) {
+            $table = 'tbl_students';
+            $primaryKey = 'id';
+            $columns = [
+                ['db' => 'id', 'dt' => 0],
+                ['db' => 'school_id', 'dt' => 1],
+                ['db' => 'name', 'dt' => 2],
+                ['db' => 'email', 'dt' => 3],
+            ];
+
+            $sql_details = [
+                'user' => 'root',
+                'pass' => 'secret',
+                'db' => 'educa_db',
+                'host' => 'mysql'
+            ];
+
+            $data = SSP::simple($request->getQueryParams(), $sql_details, $table, $primaryKey, $columns);
+
+            return $response->withJson($data);
+        })->setName('api.students');
+
+        $app->post('/students', function (Request $request, Response $response, array $args) {
+            $newStudent = $request->getParsedBody();
+
+            $student = $this->db->insert('tbl_students', [
+                'school_id' => $newStudent['school_id'],
+                'name' => $newStudent['name'],
+                'email' => $newStudent['email'],
+            ]);
+
+            if ($student->rowCount() > 0) {
+                return $response->withJson([
+                    'message' => 'Data siswa berhasil disimpan'
+                ], 201);
+            }
+
+            return $response->withJson([
+                'message' => 'Data siswa gagal disimpan'
+            ], 400);
+        })->setName('api.students.create');
+
+        $app->get('/students/{id}', function (Request $request, Response $response, array $args) {
+            $student = $this->db->select('tbl_students', '*', [
+                'id' => $args['id']
+            ]);
+
+            return $response->withJson($student[0], 200);
+        });
+
+        $app->patch('/students/{id}', function (Request $request, Response $response, array $args) {
+            $editStudent = $request->getParsedBody();
+
+            $student = $this->db->update('tbl_students', [
+                'school_id' => $editStudent['school_id'],
+                'name' => $editStudent['name'],
+                'email' => $editStudent['email'],
+            ], [
+                'id' => $args['id']
+            ]);
+
+            if ($student) {
+                return $response->withJson([
+                    'message' => 'Data siswa berhasil diubah'
+                ], 200);
+            }
+
+            return $response->withJson([
+                'message' => 'Data siswa gagal disimpan'
+            ], 400);
+        })->setName('api.students.update');
+
+        $app->delete('/students/{id}', function (Request $request, Response $response, array $args) {
+            $this->db->delete('tbl_students', [
+                'id' => $args['id']
+            ]);
+
+            return $response->withJson([
+                'message' => 'Data siswa berhasil dihapus'
+            ], 200);
+        })->setName('api.students.delete');
+    });
 };
